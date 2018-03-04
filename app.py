a5d2e55a44230ab99e40658c151a3924c9d1f040
juany-ramirez <juany.ramrz@unitec.edu>
2018-03-04 17:20:07 -0600

    Get first five

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -13,9 +14,13 @@ def home():
 def jsonf():
     return jsonify({"key":[0,1,2,3]})
 
-# @app.route('/items')
-# def get_data():
-#     site = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCcdzClrKE6FrsDce4FrnXroYcHohB1zMc&channelId=UCvS6-K6Ydmb4gH-kim3AmjA&part=snippet,id&order=date"
-#     r = requests.get(site)
-#     return r.json
+@app.route('/items')
+def get_data():
+    site = 'https://www.googleapis.com/youtube/v3/search?'
+    key = 'key='+'AIzaSyCcdzClrKE6FrsDce4FrnXroYcHohB1zMc'
+    channelId = '&channelId='+'UCvS6-K6Ydmb4gH-kim3AmjA'
+    part = '&part=snippet,id&order=date'
+    nextPageToken = '&pageToken='
+    r = requests.get(site+key+channelId+part)
+    return r.text
 
