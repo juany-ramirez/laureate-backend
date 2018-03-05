ac84c74cb4674ff79acef6fd2ad25dbbaa50fac0
juany-ramirez <juany.ramrz@unitec.edu>
2018-03-04 22:12:11 -0600

    Get data

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import requests
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
 DEVELOPER_KEY = "AIzaSyCcdzClrKE6FrsDce4FrnXroYcHohB1zMc" 
 
+
 @app.route('/')
-def home():
-    return 'Hello World'
-
-@app.route('/prueba', methods=['GET','POST'])
-def jsonf():
-    return jsonify({"key":[0,1,2,3]})
-
-@app.route('/items')
 def get_data():
     site = 'https://www.googleapis.com/youtube/v3/search?'
     key = 'key='+'AIzaSyCcdzClrKE6FrsDce4FrnXroYcHohB1zMc'
@@ -22,5 +15,13 @@ def get_data():
     part = '&part=snippet,id&order=date'
     nextPageToken = '&pageToken='
     r = requests.get(site+key+channelId+part)
-    return r.text
+    return Response(
+        r.text,
+        status=r.status_code,
+        content_type=r.headers['content-type'],
+    )
+
+@app.route('/prueba', methods=['GET','POST'])
+def jsonf():
+    return jsonify({"key":[0,1,2,3]})
 
